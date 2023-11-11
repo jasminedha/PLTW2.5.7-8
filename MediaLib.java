6e97fc43d598287dd4a4a68844e4652eec3c1bc5
jasminedha <142340759+jasminedha@users.noreply.github.com>
2023-11-10 19:56:53 -0800

    Update MediaLib.java

---
+++ MediaLib.java
@@ -0,0 +1,32 @@
+public class MediaLib
+{
+  private Book book;
+ 
+  public void addBook(Book b)
+  {
+	book = b;
+  }
+   
+  // new step 18-22
+  /* test scenario
+  public void testBook(Book tester)
+  {
+	tester.setTitle("Animal Farm");
+	// show state chagne with one of the following
+	System.out.println(" in test: " + tester);
+	System.out.println(" in test: " + tester.getTitle());
+  }
+  */
+   
+  public String toString()  
+  {
+	String info = "";
+ 
+	// Step 5, avoid crashing by testing that book is not null before using its toString method
+	if (book != null)
+  	info += "Book: " + book + "\n";
+ 
+	return info;
+  }
+}
+
